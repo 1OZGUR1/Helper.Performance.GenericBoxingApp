adf5b9238dbf7bb0423ad01e72af1b1704ba0b75
1OZGUR1 <ozguratilgan79@gmail.com>
2024-03-11 20:10:09 +0300

    Add project files.

---
+++ HelperBoxingApp/GenericBoxingUnboxing.cs
@@ -0,0 +1,8 @@
+﻿namespace HelperBoxingApp;
+
+public static class GenericBoxingUnboxing
+{
+    // This line had only one type argument
+    public static TBox? Box<TBox, TValue>(TValue value) => (TBox)Convert.ChangeType(value, typeof(TBox))!;
+    public static TValue? Unbox<TValue>(object? boxedValue) => (TValue)Convert.ChangeType(boxedValue, typeof(TValue))!;
+}
